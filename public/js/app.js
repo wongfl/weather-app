2e95c197910af6214c843b6ebc7dce0c3a5a4ffb
floteew <f.tracywong@gmail.com>
2019-08-31 20:40:48 +0800

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("JS file is running")
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
